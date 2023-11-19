3176d3840c824970893f4d681b9b236d37080e67
Programming-Seungwan <swsj4480@gmail.com>
2023-11-19 13:50:40 +0900

    20231119 백준 동적프로그래밍 파도반 수열 알고리즘 문제풀이

---
+++ Week2/BJ9184.js
@@ -0,0 +1,25 @@
+// 백준 파도반 수열 문제 #9461
+// https://www.acmicpc.net/problem/9461
+
+const fs = require('fs');
+const [n, ...arr] = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((v) => +v);
+
+const MAX = Math.max(...arr);
+
+let dp = [];
+dp[0] = 1;
+dp[1] = 1;
+dp[2] = 1;
+
+for (let i = 3; i <= MAX + 1; i++) {
+  dp[i] = dp[i - 3] + dp[i - 2];
+}
+
+arr.forEach((v) => {
+  console.log(dp[v - 1]);
+});
