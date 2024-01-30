7be05437002d0148dbab1f9e5a30e696625ca804
Programming-Seungwan <swsj4480@gmail.com>
2024-01-30 13:54:08 +0900

    BJ 2573 빙산문제 풀이
    1. dfs와 bfs를 혼용

--- 2024/Week3/BJ2573.py
+++ 2024/Week3/BJ2573.py
@@ -1,14 +1,13 @@
 # 빙산이 분리되는 최초의 시간(년 단위)을 구하는 문제
 # 빙산을 한칸 단위가 아닌, 1년 단위로 한번에 갱신해야됨, 1년이 지날 때마다 빙산이 총 몇 덩이인지 bfs로 구해줘야함
-from sys import stdin as s
-from collections import deque
-s = open('./input.txt', 'rt')
+import sys
+sys.setrecursionlimit(10 ** 6)
 
-N, M = list(map(int, s.readline().strip().split()))
+N, M = list(map(int, input().split()))
 year = 0
 islandNumber = 0
 
-seaMap = [list(map(int, s.readline().strip().split())) for _ in range(N)]
+seaMap = [list(map(int, input().split())) for _ in range(N)]
 updateMapList = [[0] * M for _ in range(N)]
 
 dY = [1, 0, -1, 0]
@@ -28,29 +27,31 @@ def countAdjacentZero(yIndex, xIndex):
   return result
 
 # 현재의 지도를 바탕으로 섬이 몇개인지를 세주는 함수
-def countIslandNumber(mapOfSea):
-  count = 0
-  check = [[False] * M for _ in range(N)]
-  def countRecur(y, x):
-    queue = deque()
-    queue.append((y, x))
-    check[y][x] = True
+def countIslandNumber(grid):
+    if not grid:
+        return 0
 
-    while queue:
-      tmpY, tmpX = queue.pop()
-      for j in range(4):
-        nextY = tmpY + dY[j]
-        nextX = tmpX + dX[j]
-        if 0 <= nextY < N and 0 <= nextX < M:
-          if check[nextY][nextX] == False:
-            queue.append((nextY, nextX))
-            check[nextY][nextX] = True
-  for k in range(N):
-    for u in range(M):
-      if mapOfSea[k][u] != 0 and check[k][u] == False:
-        countRecur(k, u)
-        count += 1
-  return count
+    rows, cols = len(grid), len(grid[0])
+    visited = [[False for _ in range(cols)] for _ in range(rows)]
+
+    def dfs(i, j):
+        if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == 0 or visited[i][j]:
+            return
+        visited[i][j] = True  # Mark as visited
+        dfs(i + 1, j)
+        dfs(i - 1, j)
+        dfs(i, j + 1)
+        dfs(i, j - 1)
+
+    count = 0
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] != 0 and not visited[i][j]:
+                dfs(i, j)
+                count += 1
+
+    return count
+
 
 while True:
   for t in range(N):
@@ -58,16 +59,17 @@ while True:
       updateMapList[t][s] = countAdjacentZero(t, s)
   for j in range(N):
     for i in range(M):
-      seaMap[j][i] = seaMap[j][i] - updateMapList[j][i] # 다음에 updateMapList를 갱신할 필요가 있는가?
+      seaMap[j][i] = seaMap[j][i] - updateMapList[j][i] if (seaMap[j][i] - updateMapList[j][i]) > 0 else 0  # 다음에 updateMapList를 갱신할 필요가 있는가?
   updateMapList = [[0] * M for _ in range(N)]
+  # print(seaMap)
   year += 1
   islandNumber = countIslandNumber(seaMap)
-  if islandNumber >= 2:
-    break
-  if islandNumber == 0:
-    break
+  # print(islandNumber)
+  # print(year)
+  if islandNumber != 1:
+     break
 
-if islandNumber > 2:
-  print(year)
-else:
-  print(0)
+if islandNumber >= 2:
+   print(year)
+if islandNumber == 0:
+   print(0)
