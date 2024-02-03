4245c85ca3f65e1ef683a01655d9a1c4d75effb9
Programming-Seungwan <swsj4480@gmail.com>
2024-02-04 00:32:51 +0900

    BJ1389 bfs 방식으로 실패 => 플로이드 와샬 알고리즘으로 풀어야 한다고 함

--- 2024/Week4/BJ1389.py
+++ 2024/Week4/BJ1389.py
@@ -3,20 +3,50 @@ from collections import deque
 s = open('./input.txt', 'rt')
 userNum, relationShipNum = list(map(int, s.readline().strip().split()))
 relationDic = {}
-visitedList = [[False] * userNum for _ in range(userNum + 1)] # 0 인덱스는 따로 사용하지 않는다
-count = 0
+visitedList = [[-1] * (userNum + 1) for _ in range(userNum + 1)] # 0 인덱스는 따로 사용하지 않는다
+
 for i in range(1, userNum + 1):
   relationDic[i] = []
 for i in range(relationShipNum):
   firstNode, secondNode = list(map(int, s.readline().strip().split()))
-  relationDic[firstNode].append(secondNode)
-  relationDic[secondNode].append(firstNode)
+  if secondNode not in relationDic[firstNode]:
+    relationDic[firstNode].append(secondNode)
+  if firstNode not in relationDic[secondNode]:
+    relationDic[secondNode].append(firstNode)
 
-def recur(originUser, newUser):
-  global count
+
+def recur(originUser):
+  count = 1
   queue = deque()
 
-  
+  for i in range(len(relationDic[originUser])):
+    queue.append(relationDic[originUser][i])
+    visitedList[originUser][relationDic[originUser][i]] = count
 
-# print(relationDic)
-# print(visitedList)
+  while queue:
+    tmpNode = queue.pop()
+
+    for i in range(len(relationDic[tmpNode])):
+      if visitedList[originUser][relationDic[tmpNode][i]] == -1:
+        visitedList[originUser][relationDic[tmpNode][i]] = visitedList[originUser][tmpNode] + 1
+        queue.append(relationDic[tmpNode][i])
+
+for i in range(userNum):
+  recur(i + 1)
+result = [0 for _ in range(userNum + 1)]
+
+for i in range(1, userNum + 1):
+  sum = 0
+  for j in range(1, userNum + 1):
+    if i != j:
+      sum += visitedList[i][j]
+  result[i] = sum
+
+resultIndex = 1
+resultSum = result[1]
+for i in range(2, userNum + 1):
+  if result[i] < resultSum:
+    resultSum = result[i]
+    resultIndex = i
+
+print(resultIndex)
