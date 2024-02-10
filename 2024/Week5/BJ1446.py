ad9ce2a03ea55b2e587ad08c136d8f368fc6dbd3
Programming-Seungwan <swsj4480@gmail.com>
2024-02-10 20:21:35 +0900

    BJ1446 문제풀이
    1. 다익스트라 알고리즘으로 문제 풀이

--- 2024/Week5/BJ1446.py
+++ 2024/Week5/BJ1446.py
@@ -2,30 +2,38 @@ from sys import stdin as s
 s = open('./input.txt', 'rt')
 routeNumber, endingPoint = list(map(int, s.readline().strip().split()))
 routeList = [list(map(int, s.readline().strip().split())) for _ in range(routeNumber)]
-# print(routeList)
-routeRealList = []
+nodeList = []
+nodeList.append(endingPoint)
+nodeList.append(0)
+for i in range(len(routeList)):
+  startNode, endNode, weight = routeList[i]
+  if startNode <= endingPoint and endNode <= endingPoint:
+    if startNode not in nodeList:
+      nodeList.append(startNode)
+    if endNode not in nodeList:
+      nodeList.append(endNode)
+nodeList.sort()
+# [0, 50, 100, 110, 140, 150]
+mapInfoDic = {}
+for i in range(len(nodeList)):
+  mapInfoDic[nodeList[i]] = {}
+  for j in range(len(nodeList)):
+    if i != j and i < j:
+      mapInfoDic[nodeList[i]][nodeList[j]] = nodeList[j] - nodeList[i]
 
 for i in range(len(routeList)):
-  startNode, endNode, length = routeList[i]
-  if startNode <= endingPoint and endNode <= endingPoint:
-    if length < (endNode - startNode):
-      if startNode not in routeRealList:
-        routeRealList.append(startNode)
-      if endNode not in routeRealList:
-        routeRealList.append(endNode)
-if 0 not in routeRealList:
-  routeRealList.append(0)
-if endingPoint not in routeRealList:
-  routeRealList.append(endingPoint)
+  startNode, endNode, weight = routeList[i]
+  if startNode in nodeList and endNode in nodeList:
+    if mapInfoDic[startNode][endNode] > weight:
+      mapInfoDic[startNode][endNode] = weight
 
-routeRealList.sort()
-routRealListLength = len(routeRealList)
-twoDimensionList = [[0] * routRealListLength for _ in range(routRealListLength)]
-for j in range(routRealListLength):
-  for i in range(routRealListLength):
-    twoDimensionList[j][i] = routeRealList[i] - routeRealList[j]
+visited = [False for _ in range(len(nodeList))]
+visited[0] = True
+for i in range(1, len(nodeList)):
+  visitNode = nodeList[i]
+  keyGroup = mapInfoDic[visitNode].keys()
+  for key in keyGroup:
+    if mapInfoDic[0][key] > mapInfoDic[0][visitNode] + mapInfoDic[visitNode][key]:
+      mapInfoDic[0][key] = mapInfoDic[0][visitNode] + mapInfoDic[visitNode][key]
 
-routeListLength = len(routeList)
-# for i in range(routeListLength):
-#   sP, eP, weight = routeList[i]
-#   if (sP in routeRealList) and (eP in routeRealList):
+print(mapInfoDic[0][endingPoint])
