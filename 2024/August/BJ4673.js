47b502017d24856f2188cf0ebbbdd738cb365e99
Programming-Seungwan <swsj4480@gmail.com>
2024-08-09 22:31:30 +0900

    Refactor : 각 자리수 합을 더하는 로직 리팩터링

--- 2024/August/BJ4673.js
+++ 2024/August/BJ4673.js
@@ -1,11 +1,21 @@
-function getResult(number) {
-  const tenThousandRemain = Math.floor(number / 10000);
-  const thousandRemain = Math.floor((number - (tenThousandRemain * 10000)) / 1000);
-  const hundredRemain = Math.floor((number - (tenThousandRemain * 10000) - (thousandRemain * 1000)) / 100);
-  const tenRemain = Math.floor((number - (tenThousandRemain * 10000) - (thousandRemain * 1000) - (hundredRemain * 100)) / 10);
-  const oneRemain = number % 10;
+// function getResult(number) {
+//   const tenThousandRemain = Math.floor(number / 10000);
+//   const thousandRemain = Math.floor((number - (tenThousandRemain * 10000)) / 1000);
+//   const hundredRemain = Math.floor((number - (tenThousandRemain * 10000) - (thousandRemain * 1000)) / 100);
+//   const tenRemain = Math.floor((number - (tenThousandRemain * 10000) - (thousandRemain * 1000) - (hundredRemain * 100)) / 10);
+//   const oneRemain = number % 10;
 
-  return number + tenThousandRemain + thousandRemain + hundredRemain + tenRemain + oneRemain
+//   return number + tenThousandRemain + thousandRemain + hundredRemain + tenRemain + oneRemain
+// }
+
+function getResult(number) {
+  let result = number;
+  const strNumber = String(number)
+  for (let i = 0; i < strNumber.length; i++) {
+    result += Number(strNumber[i])
+  }
+
+  return result
 }
 
 const resultArray = new Array(10001).fill(false)
