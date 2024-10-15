6c4b8dc1a38cf0f259d9b5633adcdcc43712a6ee
Programming-Seungwan <swsj4480@gmail.com>
2024-10-15 21:31:04 +0900

    BJ 1806 투 포인터 문제 풀이

--- 2024/October/twoPointers/retry_bj1806.js
+++ 2024/October/twoPointers/retry_bj1806.js
@@ -1,31 +1,30 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const data = fs.readFileSync(filePath).toString().split('\n');
-const [num, sum] = data[0].split(' ').map((element) => +element);
-const dataArray = data[1].split(' ').map((element) => +element);
+const inputData = fs.readFileSync(filePath).toString().split('\n');
 
-// 시작과 종료 포인터를 만들어두기.
-// 바깥의 조건문에서 항상 처음 시작과 마지막을 만들어준다.
-let flag = false;
+const [n, s] = inputData[0].split(' ').map((el) => +el);
+const dataArray = inputData[1].split(' ').map((el) => +el);
+// 왼포인터와 오른쪽 포인터는 둘다 0으로부터 시작한다
+// 사실 그냥 전체를 싹 흝으면서 점검하면 되는데, 이걸 어떻게 효율적으로 할지에 관한 문제이다
+// 오른쪽 포인터를 늘려가는 식으로 진행하는데, 왼쪽 포인터가 끝을 안넘어가면 된다.
+// 부분합이 작으면 오른쪽 포인터를 늘려서 부분합을 확장하고, 부분합이 크거나 같으면 왼 포인터를 늘린다.
+let leftPointer = 0;
+let rightPointer = 0;
+let sum = 0;
 
-for (let i = 1; i <= num; i++) {
-  let startPoint = 0;
-  let endPoint = startPoint + i;
-  while (endPoint <= num - 1) {
-    const tmpPart = dataArray.slice(startPoint, endPoint + 1);
-    const tmpPartSum = tmpPart.reduce((prev, curr) => prev + curr, 0);
+let minLength = Infinity;
 
-    if (tmpPartSum >= sum) {
-      flag = true;
-      break;
-    } else {
-      startPoint += 1;
-      endPoint += 1;
-    }
+while (rightPointer < n) {
+  sum += dataArray[rightPointer];
+
+  while (sum >= s) {
+    // 해당 while 절에서는 right가 고정된 상태에서 가장 짧은 만족하는 부분합을 만들어야 함
+    minLength = Math.min(minLength, rightPointer - leftPointer + 1);
+    sum -= dataArray[leftPointer];
+    leftPointer += 1;
   }
 
-  if (flag === true) {
-    console.log(endPoint - startPoint + 1);
-    break;
-  }
+  rightPointer += 1;
 }
+
+console.log(minLength === Infinity ? 0 : minLength);
