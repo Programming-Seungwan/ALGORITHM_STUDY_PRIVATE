326014175f937088131e4ab56d2d1c8ea0169f10
Programming-Seungwan <swsj4480@gmail.com>
2025-03-31 21:44:13 +0900

    Resolve : 우선 순위 큐를 만드는 방식으로 풀이

--- 2025/first/leetcode_k_frequent.py
+++ 2025/first/leetcode_k_frequent.py
@@ -1,4 +1,4 @@
-class Solution:
+class SortingSolution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         my_dict = {}
         for n in nums:
@@ -8,3 +8,15 @@ class Solution:
         sorted_items = sorted(my_dict.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
 
         return [item[0] for item in sorted_items[:k]]
+
+import heapq
+from typing import List
+
+class PriorityQueueSolution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        my_dict = {}
+        for n in nums:
+            my_dict[n] = my_dict.get(n, 0) + 1
+
+        # 우선 순위 큐를 사용하여 등장 횟수가 높은 k개의 요소를 추출
+        return [item[1] for item in heapq.nlargest(k, [(freq, num) for num, freq in my_dict.items()])]
