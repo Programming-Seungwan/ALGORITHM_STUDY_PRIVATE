1afc0d3c685aebae751fb552d90dbbb6d0d65fab
Programming-Seungwan <swsj4480@gmail.com>
2025-08-20 00:17:10 +0900

    solve : bj 1717 합집합 알고리즘 문제 풀이
    1. rank 개념을 활용해야 함함

--- 2025/eight/bj1717.js
+++ 2025/eight/bj1717.js
@@ -1,51 +1,63 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const [first, ...arr] = fs.readFileSync(filePath).toString().trim().split('\n');
+let input = [];
+rl.on('line', (line) => {
+  input.push(line.trim());
+}).on('close', () => {
+  const [n, m] = input[0].split(' ').map(Number);
+  const commands = input.slice(1).map((line) => line.split(' ').map(Number));
 
-const [n, m] = first.split(' ').map((item) => +item);
-const commands = arr.map((item) => item.split(' ').map((newItem) => +newItem));
-
-class UnionFind {
-  constructor(size) {
-    this.parent = Array.from({ length: size }, (_, i) => i);
-  }
-
-  find(x) {
-    if (this.parent[x] !== x) {
-      this.parent[x] = this.find(this.parent[x]);
+  class UnionFind {
+    constructor(size) {
+      this.parent = Array.from({ length: size }, (_, i) => i);
+      this.rank = Array(size).fill(1);
     }
 
-    return this.parent[x];
+    find(x) {
+      if (this.parent[x] !== x) {
+        this.parent[x] = this.find(this.parent[x]);
+      }
+      return this.parent[x];
+    }
+
+    union(x, y) {
+      const rootX = this.find(x);
+      const rootY = this.find(y);
+
+      if (rootX === rootY) return false; // Already in the same set
+
+      // Union by rank
+      if (this.rank[rootX] < this.rank[rootY]) {
+        this.parent[rootX] = rootY;
+      } else if (this.rank[rootX] > this.rank[rootY]) {
+        this.parent[rootY] = rootX;
+      } else {
+        this.parent[rootY] = rootX;
+        this.rank[rootX] += 1;
+      }
+      return true;
+    }
+
+    isInSameGroup(x, y) {
+      return this.find(x) === this.find(y);
+    }
   }
 
-  union(x, y) {
-    const rootX = this.find(x);
-    const rootY = this.find(y);
-
-    if (rootX < rootY) {
-      this.parent[rootY] = rootX;
-      return true;
-    } else if (rootX > rootY) {
-      this.parent[rootX] = rootY;
-      return true;
+  const uf = new UnionFind(n);
+  let output = [];
+  for (const command of commands) {
+    const [c, x, y] = command;
+    if (c === 0) {
+      uf.union(x, y);
     } else {
-      return false;
+      output.push(uf.isInSameGroup(x, y) ? 'YES' : 'NO');
     }
   }
+  console.log(output.join('\n'));
+});
 
-  isInSameGroup(x, y) {
-    return this.find(x) === this.find(y);
-  }
-}
-
-const uf = new UnionFind(n);
-
-for (const command of commands) {
-  const [c, x, y] = command;
-  if (c === 0) {
-    uf.union(x, y);
-  } else {
-    console.log(uf.isInSameGroup(x, y) ? 'YES' : 'NO');
-  }
-}
+// ...기존 코드 삭제, readline 내부로 이동...
